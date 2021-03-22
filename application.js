37617930fc500e394128eecae36053d2018baeaf
范启东 <fanqidong@pc-15-232.customer.ask4.lan>
2021-03-22 12:49:45 +0000

    upload vis

--- application.js
+++ application.js
@@ -15,3 +15,15 @@ function test()
 	return returnData;
 }
 console.log(select(test(),null,null,'<Country>'));
+
+var SparqlParser = require('sparqljs').Parser;
+var parser = new SparqlParser();
+var parsedQuery = parser.parse(
+  'PREFIX foaf: <http://xmlns.com/foaf/0.1/> ' +
+  'SELECT * { ?mickey foaf:name "Mickey Mouse"@en; foaf:knows ?other. }');
+
+// Regenerate a SPARQL query from a JSON object
+var SparqlGenerator = require('sparqljs').Generator;
+var generator = new SparqlGenerator({ /* prefixes, baseIRI, factory, sparqlStar */ });
+parsedQuery.variables = ['?mickey'];
+var generatedQuery = generator.stringify(parsedQuery);
